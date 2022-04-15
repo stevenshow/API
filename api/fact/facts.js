630475d8952fb5d9fcf1a629e6c678210cdd3b4e
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-04-14 22:25:28 -0600

    checks for data before posting.  gets random fact

--- api/fact/facts.js
+++ api/fact/facts.js
@@ -15,35 +15,59 @@ router.get("/", (req, res, next) => {
 });
 
 // Handle Get Request for specific type of fact
-router.get("/:type", (req, res, next) => {
-  const type = req.params.type;
-  res.status(200).json({
-    message: `Getting ${type} fact for you!`,
+router.get("/cat", (req, res, next) => {
+  Fact.countDocuments('cat', (err, count) => {
+    // Get a random entry
+    let random = Math.floor(Math.random() * count);
+
+    // give back random cat fact
+    Fact.findOne().skip(random).then((fact) => {
+      res.status(200).json({
+        fact
+      });
+    })
+    .catch((err) => {
+      console.log(err)
+    });
   });
 });
 
+// Handle posting new fact to database
 router.post("/", (req, res, next) => {
-  const fact = new Fact({
-    _id: new mongoose.Types.ObjectId(),
-    fact: req.body.fact,
-    reference: req.body.ref,
-    type: req.body.type
-
-  });
-  fact.save()
-  .then((result => {
-    console.log(result)
-    Fact.findById(result._id).exec().then((fact) => {
-      console.log("FACT: ", fact)
+  const { fact, reference, type } = req.body;
+  
+  // validate to make sure there is a fact, reference, and type present
+  // TODO deeper validation on reference
+  if (fact && reference && type) {
+    const factToPost = new Fact({
+      _id: new mongoose.Types.ObjectId(),
+      fact: req.body.fact,
+      reference: req.body.ref,
+      type: req.body.type
+  
     });
-  }))
-  .catch((err) => {
-    console.log(err);
-  });
-  res.status(201).json({
-    message: "Created Fact successfully!",
-    fact: fact
-  })
+    factToPost.save()
+    .then((result => {
+      console.log(result)
+      Fact.findById(result._id)
+      .exec()
+      .then((fact) => {
+        console.log("FACT: ", fact)
+      });
+    }))
+    .catch((err) => {
+      console.log(err);
+    });
+    res.status(201).json({
+      message: "Created Fact successfully!",
+      fact: factToPost
+    })
+  } else {
+      res.status(400).json({
+        message: "Make sure all fields are present"
+      })
+  }
+
 })
 
 module.exports = router;
